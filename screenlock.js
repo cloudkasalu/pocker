2b5e5f767044419ea857642aaa0776df14d933ce
Cloud <65218174+cloudkasalu@users.noreply.github.com>
2023-04-05 23:28:19 +0200

    lockscreen test5

--- screenlock.js
+++ screenlock.js
@@ -1,4 +1,9 @@
 const lockSreen = ()=>{
+    const container = document.querySelector("#gameBoard");
+    container.requestFullscreen().catch((error) => {
+      console.log( `${error}\n`);
+    });
+    
     const oppositeOrientation = screen.orientation.type.startsWith("portrait")
     ? "landscape"
     : "portrait";
